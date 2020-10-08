f964d84ab4d6e6d18eb8bf109379fd0dfd588282
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:58:29 +0200

    Tomando snapshot de un ejercicio terminado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
+// https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
+
+// Personal note: At the time I took the course, I had React v.16.13.1
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    
+    componentDidMount() {
+        // GET the data I need to correctly display
+    }
+    
+    //Looks like this one will be deprecated on react v.17
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.whatever !== this.props.whatever) {
+            // do something important here
+        }
+    }
+    
+    shouldComponentUpdate(nextProps, nextState) {
+        // return true if want it to update
+        // return false if not
+    }
+    
+    componentWillUnmount() {
+        // teardown or cleanup your code before your component disappears
+        // (E.g. remove event listeners)
+    }
+
+    render() {
+        return (
+            <div>
+                Code goes here
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
